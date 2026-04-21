f11c044e5cad36266d0ef54e184855864d94679a
Amaan Qureshi <git@amaanq.com>
2026-04-21 15:23:53 -0400

    nix/checks: bump VM test memory and cores
    The nixosTest default of 1 GiB / 1 core OOMs the backdoor shell on CI
    and surfaces as "Shell disconnected".

--- nix/checks/default.nix
+++ nix/checks/default.nix
@@ -7,6 +7,11 @@ self: {pkgs}: let
         documentation.enable = pkgs.lib.mkDefault false;
         # virtio_pci must be loaded explicitly so udev can discover virtio_blk.
         boot.initrd.kernelModules = ["virtio_pci" "virtio_blk"];
+
+        # The nixosTest default of 1 GiB / 1 core OOMs the backdoor shell
+        # on CI and surfaces as "Shell disconnected".
+        virtualisation.memorySize = 2048;
+        virtualisation.cores = 2;
       };
     })
     .config
